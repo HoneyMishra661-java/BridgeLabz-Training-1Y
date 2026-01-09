c650d10d90c53ebcd53d5170b1d2c9457e570e12
HoneyMishra661-java <honeymishra72006@gmail.com>
2026-01-09 15:12:50 +0530

    Added: Basic program of programming construct.

---
+++ basicprograms/ctof.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ctof {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user for Celsius value
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = input.nextDouble();
+
+        // Perform the conversion
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        // Display the result
+        System.out.println(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
+        
+        input.close();
+    }
+}
