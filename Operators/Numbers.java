16da72862f97475520cbd965b1e7ba6324beb70e
HoneyMishra661-java <honeymishra72006@gmail.com>
2026-01-28 09:46:22 +0530

    Added Operators for WorkShop.

---
+++ Operators/Numbers.java
@@ -0,0 +1,9 @@
+public class Numbers {
+    public static void main (String[] args){
+        int i = 1 ;
+        while (i<=20) {
+            System.out.printf(i + " " );
+            i++;
+        }
+    }    
+}
