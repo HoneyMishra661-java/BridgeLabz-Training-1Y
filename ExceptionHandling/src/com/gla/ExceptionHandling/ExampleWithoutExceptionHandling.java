7458499ad490e73dfaf2d668c0539a9c679f4f1d
HoneyMishra661-java <honeymishra72006@gmail.com>
2026-03-17 15:48:12 +0530

    Added Excetpion program for workshop

---
+++ ExceptionHandling/src/com/gla/ExceptionHandling/ExampleWithoutExceptionHandling.java
@@ -0,0 +1,10 @@
+package com.gla.ExceptionHandling;
+
+public class ExampleWithoutExceptionHandling {
+    public static void main (String[] args){
+        int a = 10 , b = 0 ;
+        int c = a / b ;
+        System.out.println("Result : " + c );
+    }
+}
+    
